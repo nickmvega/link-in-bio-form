135634c48cd3b9eb53a6a9deaa9df94263234cdf
Nicholas Vega <122232969+nickmvega@users.noreply.github.com>
2024-11-05 23:24:12 +0000

    in class

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,19 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def form 
+    render({ :template => "item_templates/form"})
+  end 
+
+  def insert
+    item = Item.new
+    item.link_url = params[:query_link_url]
+    item.link_description = params[:query_link_description]
+    item.thumbnail_url = params[:query_thumbnail_url]
+
+    item.save
+    
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "form"})
+
+  post("/insert_item", { :controller => "items", :action => "insert"})
 end
